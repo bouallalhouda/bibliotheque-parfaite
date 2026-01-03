ac7cbf6a6462a47cb12d638eade50a1b4d1a8162
Aya949 <aitallalaya2335@gmail.com>
2026-01-03 23:24:59 +0100

    Add EmpruntDAO interface with CRUD methods

--- src/main/java/com/bibliotheque/dao/EmpruntDAO.java
+++ src/main/java/com/bibliotheque/dao/EmpruntDAO.java
@@ -1,1 +1,19 @@
 package com.bibliotheque.dao;
+
+import com.bibliotheque.model.Emprunt;
+import java.util.List;
+import java.util.Optional;
+
+public interface EmpruntDAO extends DAO<Emprunt> {
+    // Optional findById is inherited from DAO<T>, redeclared here for clarity
+    Optional<Emprunt> findById(int id);
+
+    void save(Emprunt emprunt);
+    List<Emprunt> findAll();
+    void update(Emprunt emprunt);
+    void delete(int id);
+
+    List<Emprunt> findByLivreIsbn(String isbn);
+    List<Emprunt> findByMembre(int membreId);
+    List<Emprunt> findEmpruntsEnCours();
+}
