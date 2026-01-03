43d85245f8876367add8c8012d05d611bc0c3a2b
ayalarhlimi <ayalarhlimi46@gmail.com>
2026-01-03 15:56:10 +0100

    ajout de la classe membre

--- src/main/java/com/bibliotheque/model/Membre.java
+++ src/main/java/com/bibliotheque/model/Membre.java
@@ -1,19 +1,18 @@
 package com.bibliotheque.model;
-package model;
 
 public class Membre extends Personne {
+
     private int id;
     private boolean actif;
 
     public Membre(int id, String nom, String prenom, String email, boolean actif) {
-        super(nom, prenom, email); // appel du parent
+        super(nom, prenom, email);
         this.id = id;
         this.actif = actif;
     }
 
     public int getId() { return id; }
-
     public boolean isActif() { return actif; }
-
     public void setActif(boolean actif) { this.actif = actif; }
 }
+
