8ea042433e6b03ddb270e80e026d47b459e72105
ayalarhlimi <ayalarhlimi46@gmail.com>
2026-01-03 15:57:26 +0100

    Add MembreDAO interface with CRUD methods

--- src/main/java/com/bibliotheque/dao/MembreDAO.java
+++ src/main/java/com/bibliotheque/dao/MembreDAO.java
@@ -1,1 +1,22 @@
 package com.bibliotheque.dao;
+
+import com.bibliotheque.model.Membre;
+import java.util.List;
+
+public interface MembreDAO {
+
+    void save(Membre membre);
+
+    Membre findById(int id);
+
+    List<Membre> findAll();
+
+    void update(Membre membre);
+
+    void delete(int id);
+
+    Membre findByEmail(String email);
+
+    List<Membre> findActifs();
+}
+
