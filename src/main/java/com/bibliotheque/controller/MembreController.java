347f5025a3c125f693fa2c8e3bec94fcdbe63896
ayalarhlimi <ayalarhlimi46@gmail.com>
2026-01-03 16:14:37 +0100

    Implement MembreController for member management
    Added MembreController to manage member operations.

--- src/main/java/com/bibliotheque/controller/MembreController.java
+++ src/main/java/com/bibliotheque/controller/MembreController.java
@@ -1,1 +1,37 @@
+package com.bibliotheque.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import com.bibliotheque.model.Membre;
+import com.bibliotheque.service.BibliothequeService;
+import com.bibliotheque.dao.impl.MembreDAOImpl;
+
+public class MembreController {
+
+    private BibliothequeService service = new BibliothequeService(new MembreDAOImpl());
+
+    @FXML
+    private TextField nomField;
+
+    @FXML
+    private TextField prenomField;
+
+    @FXML
+    private TextField emailField;
+
+    @FXML
+    private Button ajouterButton;
+
+    @FXML
+    private void ajouterMembre() {
+        try {
+            Membre m = new Membre(0, nomField.getText(), prenomField.getText(), emailField.getText(), true);
+            service.ajouterMembre(m);
+            System.out.println("Membre ajouté !");
+        } catch (Exception e) {
+            System.out.println("Erreur : " + e.getMessage());
+        }
+    }
+}
 // Controller membres 
