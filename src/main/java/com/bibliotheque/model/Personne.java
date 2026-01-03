c8f45860bf3e4e2803343db3dd9c9f0c609cecb0
ayalarhlimi <ayalarhlimi46@gmail.com>
2026-01-03 15:53:32 +0100

    Update Personne.java

--- src/main/java/com/bibliotheque/model/Personne.java
+++ src/main/java/com/bibliotheque/model/Personne.java
@@ -1,5 +1,4 @@
 package com.bibliotheque.model;
-package model;
 
 public abstract class Personne {
     protected String nom;
@@ -12,14 +11,9 @@ public abstract class Personne {
         this.email = email;
     }
 
-    // Getters & Setters
     public String getNom() { return nom; }
-    public void setNom(String nom) { this.nom = nom; }
-
     public String getPrenom() { return prenom; }
-    public void setPrenom(String prenom) { this.prenom = prenom; }
-
     public String getEmail() { return email; }
-    public void setEmail(String email) { this.email = email; }
 }
+
 
