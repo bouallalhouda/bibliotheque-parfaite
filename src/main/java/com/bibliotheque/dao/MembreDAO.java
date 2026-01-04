2c03fee7e110ed0125c8d93d9536014c505237c7
ayalarhlimi <ayalarhlimi46@gmail.com>
2026-01-04 20:20:22 +0100

    Update MembreDAO.java

--- src/main/java/com/bibliotheque/dao/MembreDAO.java
+++ src/main/java/com/bibliotheque/dao/MembreDAO.java
@@ -5,14 +5,9 @@ import java.util.List;
 import java.util.Optional;
 
 public interface MembreDAO extends DAO<Membre> {
-    // Optional findById is inherited from DAO<T>, redeclared here for clarity
-    Optional<Membre> findById(int id);
-
-    void save(Membre membre);
-    List<Membre> findAll();
-    void update(Membre membre);
-    boolean delete(int id);
-
+    
     Membre findByEmail(String email);
+    
+   
     List<Membre> findActifs();
 }
