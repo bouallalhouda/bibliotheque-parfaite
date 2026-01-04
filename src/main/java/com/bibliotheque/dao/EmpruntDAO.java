efed299296f214e35697cfca4c2425d621e3730c
Aya949 <aitallalaya2335@gmail.com>
2026-01-04 22:52:37 +0100

    Update EmpruntDAO.java

--- src/main/java/com/bibliotheque/dao/EmpruntDAO.java
+++ src/main/java/com/bibliotheque/dao/EmpruntDAO.java
@@ -2,18 +2,14 @@ package com.bibliotheque.dao;
 
 import com.bibliotheque.model.Emprunt;
 import java.util.List;
-import java.util.Optional;
 
 public interface EmpruntDAO extends DAO<Emprunt> {
-    // Optional findById is inherited from DAO<T>, redeclared here for clarity
-    Optional<Emprunt> findById(int id);
-
-    void save(Emprunt emprunt);
-    List<Emprunt> findAll();
-    void update(Emprunt emprunt);
-    boolean delete(int id);
-
-    List<Emprunt> findByLivreIsbn(String isbn);
-    List<Emprunt> findByMembre(int membreId);
-    List<Emprunt> findEmpruntsEnCours();
+    // Récupère tous les emprunts d'un membre et retourne une liste des Emprunts du membre
+    List<Emprunt> findByMembre(int idMembre);
+    
+    // Récupère tous les emprunts en cours et retourne une liste des Emprunts en cours
+    List<Emprunt> findEnCours();
+    
+    // Compte le nombre d'emprunts en cours d'un membre et retourne le nombre d'emprunts en cours
+    int countEmpruntsEnCours(int idMembre);
 }
