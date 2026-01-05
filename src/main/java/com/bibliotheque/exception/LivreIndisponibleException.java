8f8858e6ffdd409b8ff6a5e7bdca7165c6563027
bouallal houda <bouallalhouda125@gmail.com>
2026-01-05 01:51:50 +0100

    Enhance LivreIndisponibleException with cause handling
    Added a constructor to handle exceptions with a cause.

--- src/main/java/com/bibliotheque/exception/LivreIndisponibleException.java
+++ src/main/java/com/bibliotheque/exception/LivreIndisponibleException.java
@@ -1,8 +1,12 @@
-// Exception livre indisponible 
+// Exception livre indisponible - pour signaler qu'un livre n'est pas disponible (déjà emprunté ou inexistant)
 package com.bibliotheque.exception;
 
 public class LivreIndisponibleException extends Exception {
     public LivreIndisponibleException(String message) {
         super(message);
+    }
+    
+    public LivreIndisponibleException(String message, Throwable cause) {
+        super(message, cause);
     }
 }
