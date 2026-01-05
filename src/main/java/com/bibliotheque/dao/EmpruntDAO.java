7fbee989fa9530f789e68c930d2b672c8c590680
Aya949 <aitallalaya2335@gmail.com>
2026-01-05 12:40:50 +0100

    Update EmpruntDAO.java

--- src/main/java/com/bibliotheque/dao/EmpruntDAO.java
+++ src/main/java/com/bibliotheque/dao/EmpruntDAO.java
@@ -4,12 +4,7 @@ import com.bibliotheque.model.Emprunt;
 import java.util.List;
 
 public interface EmpruntDAO extends DAO<Emprunt> {
-    // Récupère tous les emprunts d'un membre et retourne une liste des Emprunts du membre
     List<Emprunt> findByMembre(int idMembre);
-    
-    // Récupère tous les emprunts en cours et retourne une liste des Emprunts en cours
     List<Emprunt> findEnCours();
-    
-    // Compte le nombre d'emprunts en cours d'un membre et retourne le nombre d'emprunts en cours
     int countEmpruntsEnCours(int idMembre);
 }
